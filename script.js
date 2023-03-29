988540ac3736a42293a611996ea52949c095f675
aniketjadhav1812 <128202035+aniketjadhav1812@users.noreply.github.com>
2023-03-29 15:33:30 +0530

    asad

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
-        
-        // scroll-up button show/hide script
+    
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -17,12 +16,10 @@ $(document).ready(function(){
 
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
