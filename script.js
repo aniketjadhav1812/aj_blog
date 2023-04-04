01431e6e3f651bc3a1f0605dba63c7ddcd169bc7
aniketjadhav1812 <128202035+aniketjadhav1812@users.noreply.github.com>
2023-04-04 19:14:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,6 +41,29 @@ $(document).ready(function(){
         backSpeed: 40,
         loop: true
     });
+    
+       $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
 
     function warning(){
         alert("Projects are not uploaded yet....!");
